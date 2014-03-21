a4616d8ebbca63a46426cd5ba785f5d7f0316d87
Peter Reavy <peter.reavy@gmail.com>
2014-03-21 22:41:42 +0000

    Add a POST route; update README.

--- src/grab/bag.clj
+++ src/grab/bag.clj
@@ -1,9 +1,10 @@
 (ns grab.bag
-  (:require [compojure.core :refer [defroutes GET]]
+  (:require [compojure.core :refer [defroutes GET POST]]
             [ring.adapter.jetty :as jetty]))
 
 (defroutes routes
-  (GET "/" [] "<h2>Grab Bag</h2>"))
+  (GET "/" [] "<h2>Grab Bag</h2>")
+  (POST "/hey" [] "reply"))
 
 (defn -main [port]
   (jetty/run-jetty #'routes {:port (Integer. port) :join? false}))
