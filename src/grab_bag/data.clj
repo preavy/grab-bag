030eeb682c3850ccebd95fee8500677f0c8f950e
Peter Reavy <peter.reavy@gmail.com>
2014-04-18 23:11:24 +0100

    Completed rename.

--- src/grab_bag/data.clj
+++ src/grab_bag/data.clj
@@ -1,4 +1,4 @@
-(ns grab.data)
+(ns grab-bag.data)
 
 (def people '[:a :b :c :d :e :f :g :h :i :j :k :l :m :o :n :p :q :r :s :t :u :v :w :x :y :z])
 
--- src/grab_bag/server.clj
+++ src/grab_bag/server.clj
@@ -1,15 +1,15 @@
-(ns grab.bag
+(ns grab-bag.server
   (:require [compojure.core :refer [defroutes GET POST]]
             [compojure.handler :as handler]
             [ring.adapter.jetty :as jetty]
-            [grab.data :as data]))
+            [grab-bag.data :as data]))
 
 (defn render-status []
   (str @data/choices))
 
 (defroutes routes
   (GET "/" [] (str "<h2>Grabbe Bagge</h2>" (render-status)))
-  (POST "/hey" [person choice] (swap! grab.data/choices grab.data/grab (keyword person) (keyword choice))))
+  (POST "/hey" [person choice] (swap! grab-bag.data/choices grab-bag.data/grab (keyword person) (keyword choice))))
 
 (def my-handler (handler/site routes))
 
